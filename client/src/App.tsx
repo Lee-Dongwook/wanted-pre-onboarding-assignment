f390f34aaba0c383ea46ae330e4035e27d93e012
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-02-26 19:45:28 +0900

    feat : Input Add Delete 기능 동작 init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState, type ChangeEvent } from "react";
+
+interface List {
+  idx: number;
+  title: string;
+}
 
 function App() {
-  return <div className="text-3xl font-bold underline">Hello!</div>;
+  const [todoTitle, setTodoTitle] = useState<string>("");
+  const [todoList, setTodoList] = useState<List[]>([]);
+
+  const onChangeTodoTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    setTodoTitle(e.target.value);
+  };
+
+  const handleAddTodoList = () => {
+    try {
+      const newTodoTitle = todoTitle;
+      setTodoList((prevList) => [
+        ...prevList,
+        { idx: todoList.length, title: newTodoTitle },
+      ]);
+      setTodoTitle("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDeleteTodoList = (idx: number) => {
+    try {
+      const deleteIndex = idx;
+      const updatedTodoList = todoList.filter(
+        (todo) => todo.idx !== deleteIndex
+      );
+      setTodoList(updatedTodoList);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={todoTitle}
+        onChange={onChangeTodoTitle}
+        placeholder="input"
+      />
+      <button onClick={handleAddTodoList}>Add</button>
+      {todoList.map((todo: List) => {
+        return (
+          <div key={todo.idx}>
+            <strong>{todo.title}</strong>
+            <button onClick={() => handleDeleteTodoList(todo.idx)}>
+              Delete
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
