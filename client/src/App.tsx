69bba4d21095afb55a1555c56192257918961eff
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-02-26 19:02:11 +0900

    style : tailwindcss 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Hello!</div>;
+  return <div className="text-3xl font-bold underline">Hello!</div>;
 }
 
 export default App;
